996fb740f3b545820fc6f825993f3d4b0db13b30
berkayalkan <brkyalkn13@gmail.com>
2021-05-05 20:51:11 +0300

    Add training data

--- ReviewClassifier/updater.py
+++ ReviewClassifier/updater.py
@@ -16,77 +16,401 @@ def json_helper(review_list, rating, cat_type):
 if __name__ == '__main__':
     with open("reviews.pickle", "rb") as p:
         reviews_pickle = pickle.load(p)
-    bug_1 = [reviews_pickle[1][301], reviews_pickle[1][305], reviews_pickle[1][312], reviews_pickle[1][313],
-             reviews_pickle[1][315], reviews_pickle[1][320], reviews_pickle[1][324], reviews_pickle[1][326],
-             reviews_pickle[1][334], reviews_pickle[1][335], reviews_pickle[1][338], reviews_pickle[1][339],
-             reviews_pickle[1][342], reviews_pickle[1][341], reviews_pickle[1][343], reviews_pickle[1][345],
-             reviews_pickle[1][347], reviews_pickle[1][363], reviews_pickle[1][367], reviews_pickle[1][372],
-             reviews_pickle[1][376], reviews_pickle[1][383], reviews_pickle[1][390], reviews_pickle[1][391],
-             reviews_pickle[1][393], reviews_pickle[1][401]]
-    bug_3 = [reviews_pickle[3][91]]
-    bug_4 = [reviews_pickle[4][216], reviews_pickle[4][221], reviews_pickle[4][227], reviews_pickle[4][240],
-             reviews_pickle[4][241], reviews_pickle[4][245]]
+    bug_1 = [
+        reviews_pickle[1][8],
+        reviews_pickle[1][14],
+        reviews_pickle[1][17],
+        reviews_pickle[1][19],
+        reviews_pickle[1][24],
+        reviews_pickle[1][28],
+        reviews_pickle[1][31],
+        reviews_pickle[1][33],
+        reviews_pickle[1][34],
+        reviews_pickle[1][35],
+        reviews_pickle[1][37],
+        reviews_pickle[1][43],
+        reviews_pickle[1][46],
+        reviews_pickle[1][47],
+        reviews_pickle[1][51],
+        reviews_pickle[1][52],
+        reviews_pickle[1][55],
+        reviews_pickle[1][68],
+        reviews_pickle[1][69],
+        reviews_pickle[1][194],
+        reviews_pickle[1][197],
+        reviews_pickle[1][198],
+        reviews_pickle[1][206],
+        reviews_pickle[1][211],
+        reviews_pickle[1][218],
+        reviews_pickle[1][222],
+        reviews_pickle[1][225],
+        reviews_pickle[1][229],
+        reviews_pickle[1][232],
+        reviews_pickle[1][233],
+        reviews_pickle[1][235],
+        reviews_pickle[1][246],
+        reviews_pickle[1][247],
+        reviews_pickle[1][248],
+        reviews_pickle[1][251],
+        reviews_pickle[1][259],
+        reviews_pickle[1][261],
+        reviews_pickle[1][264],
+        reviews_pickle[1][282],
+        reviews_pickle[1][283]
+    ]
+    bug_2 = [
+        reviews_pickle[2][2],
+        reviews_pickle[2][3],
+        reviews_pickle[2][5],
+        reviews_pickle[2][22],
+        reviews_pickle[2][24],
+        reviews_pickle[2][28],
+        reviews_pickle[2][36],
+        reviews_pickle[2][37],
+        reviews_pickle[2][40],
+        reviews_pickle[2][42],
+        reviews_pickle[2][44],
+        reviews_pickle[2][45],
+        reviews_pickle[2][46]
+    ]
+    bug_3 = [
+        reviews_pickle[3][3],
+        reviews_pickle[3][32],
+        reviews_pickle[3][18],
+        reviews_pickle[3][63],
+        reviews_pickle[3][70],
+        reviews_pickle[3][74]
+    ]
+    bug_4 = [
+        reviews_pickle[4][149],
+        reviews_pickle[4][164],
+        reviews_pickle[4][175]
+    ]
 
-    feature_1 = [reviews_pickle[1][303], reviews_pickle[1][304], reviews_pickle[1][308], reviews_pickle[1][323],
-                 reviews_pickle[1][364], reviews_pickle[1][365], reviews_pickle[1][377], reviews_pickle[1][380],
-                 reviews_pickle[1][381], reviews_pickle[1][385], reviews_pickle[1][396]]
-    feature_3 = [reviews_pickle[3][78], reviews_pickle[3][79], reviews_pickle[3][90]]
-    feature_4 = [reviews_pickle[4][211], reviews_pickle[4][214],
-                 reviews_pickle[4][217], reviews_pickle[4][219], reviews_pickle[4][224], reviews_pickle[4][225],
-                 reviews_pickle[4][229], reviews_pickle[4][230], reviews_pickle[4][232], reviews_pickle[4][233],
-                 reviews_pickle[4][237], reviews_pickle[4][238], reviews_pickle[4][242], reviews_pickle[4][247],
-                 reviews_pickle[4][252], reviews_pickle[4][267], reviews_pickle[4][268], reviews_pickle[4][272],
-                 reviews_pickle[4][279]]
+    feature_1 = [
+        reviews_pickle[1][12],
+        reviews_pickle[1][18],
+        reviews_pickle[1][21],
+        reviews_pickle[1][38],
+        reviews_pickle[1][40],
+        reviews_pickle[1][199],
+        reviews_pickle[1][204],
+        reviews_pickle[1][208],
+        reviews_pickle[1][227],
+        reviews_pickle[1][231],
+        reviews_pickle[1][244],
+        reviews_pickle[1][253],
+        reviews_pickle[1][265],
+        reviews_pickle[1][266],
+        reviews_pickle[1][272],
+        reviews_pickle[1][277],
+        reviews_pickle[1][292]
+    ]
+    feature_2 = [
+        reviews_pickle[2][0],
+        reviews_pickle[2][1],
+        reviews_pickle[2][6],
+        reviews_pickle[2][12],
+        reviews_pickle[2][20],
+        reviews_pickle[2][32],
+        reviews_pickle[2][47]
+    ]
+    feature_3 = [
+        reviews_pickle[3][0],
+        reviews_pickle[3][5],
+        reviews_pickle[3][6],
+        reviews_pickle[3][8],
+        reviews_pickle[3][9],
+        reviews_pickle[3][11],
+        reviews_pickle[3][13],
+        reviews_pickle[3][20],
+        reviews_pickle[3][25],
+        reviews_pickle[3][26],
+        reviews_pickle[3][30],
+        reviews_pickle[3][31],
+        reviews_pickle[3][37],
+        reviews_pickle[3][58],
+        reviews_pickle[3][66]
+    ]
+    feature_4 = [
+        reviews_pickle[4][159],
+        reviews_pickle[4][184],
+        reviews_pickle[4][193],
+        reviews_pickle[4][202],
+        reviews_pickle[4][162],
+        reviews_pickle[4][167],
+        reviews_pickle[4][173]
+    ]
+    feature_5 = [
+        reviews_pickle[5][1070],
+        reviews_pickle[5][1079]
+    ]
 
-    user_ex_1 = [reviews_pickle[1][301], reviews_pickle[1][302], reviews_pickle[1][306], reviews_pickle[1][307],
-                 reviews_pickle[1][309], reviews_pickle[1][311], reviews_pickle[1][314], reviews_pickle[1][316],
-                 reviews_pickle[1][318], reviews_pickle[1][319], reviews_pickle[1][322], reviews_pickle[1][327],
-                 reviews_pickle[1][329], reviews_pickle[1][340], reviews_pickle[1][344], reviews_pickle[1][346],
-                 reviews_pickle[1][348], reviews_pickle[1][350], reviews_pickle[1][351], reviews_pickle[1][352],
-                 reviews_pickle[1][354], reviews_pickle[1][357], reviews_pickle[1][361], reviews_pickle[1][362],
-                 reviews_pickle[1][366], reviews_pickle[1][349], reviews_pickle[1][369], reviews_pickle[1][370],
-                 reviews_pickle[1][373], reviews_pickle[1][375], reviews_pickle[1][378], reviews_pickle[1][382],
-                 reviews_pickle[1][388], reviews_pickle[1][392], reviews_pickle[1][393], reviews_pickle[1][399],
-                 reviews_pickle[1][400]]
-    user_ex_3 = [reviews_pickle[3][81], reviews_pickle[3][83], reviews_pickle[3][85], reviews_pickle[3][86],
-                 reviews_pickle[3][89]]
-    user_ex_4 = [reviews_pickle[4][209], reviews_pickle[4][218], reviews_pickle[4][233], reviews_pickle[4][249],
-                 reviews_pickle[4][251], reviews_pickle[4][253], reviews_pickle[4][255], reviews_pickle[4][257],
-                 reviews_pickle[4][258], reviews_pickle[4][261], reviews_pickle[4][270], reviews_pickle[4][276],
-                 ]
+    user_ex_1 = [
+        reviews_pickle[1][0],
+        reviews_pickle[1][1],
+        reviews_pickle[1][4],
+        reviews_pickle[1][5],
+        reviews_pickle[1][10],
+        reviews_pickle[1][17],
+        reviews_pickle[1][22],
+        reviews_pickle[1][29],
+        reviews_pickle[1][30],
+        reviews_pickle[1][44],
+        reviews_pickle[1][49],
+        reviews_pickle[1][54],
+        reviews_pickle[1][57],
+        reviews_pickle[1][60],
+        reviews_pickle[1][62],
+        reviews_pickle[1][67],
+        reviews_pickle[1][75],
+        reviews_pickle[1][78],
+        reviews_pickle[1][79],
+        reviews_pickle[1][192],
+        reviews_pickle[1][195],
+        reviews_pickle[1][196],
+        reviews_pickle[1][200],
+        reviews_pickle[1][205],
+        reviews_pickle[1][207],
+        reviews_pickle[1][214],
+        reviews_pickle[1][215],
+        reviews_pickle[1][216],
+        reviews_pickle[1][230],
+        reviews_pickle[1][239],
+        reviews_pickle[1][242],
+        reviews_pickle[1][243],
+        reviews_pickle[1][244],
+        reviews_pickle[1][252],
+        reviews_pickle[1][255],
+        reviews_pickle[1][258],
+        reviews_pickle[1][267],
+        reviews_pickle[1][268],
+        reviews_pickle[1][269],
+        reviews_pickle[1][281],
+        reviews_pickle[1][293],
+        reviews_pickle[1][294]
+    ]
+    user_ex_2 = [
+        reviews_pickle[2][3],
+        reviews_pickle[2][4],
+        reviews_pickle[2][7],
+        reviews_pickle[2][9],
+        reviews_pickle[2][10],
+        reviews_pickle[2][13],
+        reviews_pickle[2][16],
+        reviews_pickle[2][19],
+        reviews_pickle[2][21],
+        reviews_pickle[2][23],
+        reviews_pickle[2][38],
+        reviews_pickle[2][39],
+        reviews_pickle[2][43]
+    ]
+    user_ex_3 = [
+        reviews_pickle[3][4],
+        reviews_pickle[3][7],
+        reviews_pickle[3][14],
+        reviews_pickle[3][19],
+        reviews_pickle[3][21],
+        reviews_pickle[3][29],
+        reviews_pickle[3][34],
+        reviews_pickle[3][35],
+        reviews_pickle[3][38],
+        reviews_pickle[3][60],
+        reviews_pickle[3][61],
+        reviews_pickle[3][64],
+        reviews_pickle[3][65],
+        reviews_pickle[3][73],
+        reviews_pickle[3][74]
+    ]
+    user_ex_4 = [
+        reviews_pickle[4][126],
+        reviews_pickle[4][128],
+        reviews_pickle[4][130],
+        reviews_pickle[4][131],
+        reviews_pickle[4][137],
+        reviews_pickle[4][146],
+        reviews_pickle[4][153],
+        reviews_pickle[4][185],
+        reviews_pickle[4][188],
+        reviews_pickle[4][191],
+        reviews_pickle[4][199],
+        reviews_pickle[4][200],
+        reviews_pickle[4][204],
+        reviews_pickle[4][170],
+        reviews_pickle[4][171],
+        reviews_pickle[4][176],
+        reviews_pickle[4][178]
+    ]
+    user_ex_5 = [
+        reviews_pickle[5][1004],
+        reviews_pickle[5][1007],
+        reviews_pickle[5][1011],
+        reviews_pickle[5][1021],
+        reviews_pickle[5][1022],
+        reviews_pickle[5][1025],
+        reviews_pickle[5][1031],
+        reviews_pickle[5][1034],
+        reviews_pickle[5][1050],
+        reviews_pickle[5][1055],
+        reviews_pickle[5][1085],
+        reviews_pickle[5][1092]
+    ]
 
-    ratings_1 = [reviews_pickle[1][300], reviews_pickle[1][310], reviews_pickle[1][321], reviews_pickle[1][325],
-                 reviews_pickle[1][333], reviews_pickle[1][336], reviews_pickle[1][337], reviews_pickle[1][343],
-                 reviews_pickle[1][333], reviews_pickle[1][349], reviews_pickle[1][353], reviews_pickle[1][355],
-                 reviews_pickle[1][356], reviews_pickle[1][359], reviews_pickle[1][368], reviews_pickle[1][374],
-                 reviews_pickle[1][384], reviews_pickle[1][387], reviews_pickle[1][389], reviews_pickle[1][394],
-                 reviews_pickle[1][396], reviews_pickle[1][402]]
-    ratings_3 = [reviews_pickle[3][76], reviews_pickle[3][77], reviews_pickle[3][80], reviews_pickle[3][87],
-                 reviews_pickle[3][88]]
-    ratings_4 = [reviews_pickle[4][205], reviews_pickle[4][206], reviews_pickle[4][207], reviews_pickle[4][208],
-                 reviews_pickle[4][210], reviews_pickle[4][212], reviews_pickle[4][213], reviews_pickle[4][215],
-                 reviews_pickle[4][218], reviews_pickle[4][220], reviews_pickle[4][223], reviews_pickle[4][224],
-                 reviews_pickle[4][228], reviews_pickle[4][226], reviews_pickle[4][231], reviews_pickle[4][234],
-                 reviews_pickle[4][235], reviews_pickle[4][239], reviews_pickle[4][243], reviews_pickle[4][244],
-                 reviews_pickle[4][246], reviews_pickle[4][248], reviews_pickle[4][250], reviews_pickle[4][251],
-                 reviews_pickle[4][252], reviews_pickle[4][254], reviews_pickle[4][256], reviews_pickle[4][259],
-                 reviews_pickle[4][260], reviews_pickle[4][262], reviews_pickle[4][263], reviews_pickle[4][264],
-                 reviews_pickle[4][265], reviews_pickle[4][266], reviews_pickle[4][269], reviews_pickle[4][270],
-                 reviews_pickle[4][271], reviews_pickle[4][273], reviews_pickle[4][274], reviews_pickle[4][275],
-                 reviews_pickle[4][278], reviews_pickle[4][280], reviews_pickle[4][281]]
+    ratings_1 = [
+        reviews_pickle[1][23],
+        reviews_pickle[1][25],
+        reviews_pickle[1][26],
+        reviews_pickle[1][30],
+        reviews_pickle[1][27],
+        reviews_pickle[1][33],
+        reviews_pickle[1][39],
+        reviews_pickle[1][48],
+        reviews_pickle[1][51],
+        reviews_pickle[1][54],
+        reviews_pickle[1][58],
+        reviews_pickle[1][61],
+        reviews_pickle[1][62],
+        reviews_pickle[1][64],
+        reviews_pickle[1][67],
+        reviews_pickle[1][75],
+        reviews_pickle[1][208],
+        reviews_pickle[1][209],
+        reviews_pickle[1][214],
+        reviews_pickle[1][224],
+        reviews_pickle[1][25],
+        reviews_pickle[1][402],
+        reviews_pickle[1][249],
+        reviews_pickle[1][254],
+        reviews_pickle[1][257],
+        reviews_pickle[1][258],
+        reviews_pickle[1][261],
+        reviews_pickle[1][262],
+        reviews_pickle[1][266],
+        reviews_pickle[1][270],
+        reviews_pickle[1][61],
+        reviews_pickle[1][25],
+        reviews_pickle[1][298]
+    ]
+    ratings_2 = [
+        reviews_pickle[2][15],
+        reviews_pickle[2][25],
+        reviews_pickle[2][35],
+        reviews_pickle[2][41]
+    ]
+    ratings_3 = [
+        reviews_pickle[3][2],
+        reviews_pickle[3][8],
+        reviews_pickle[3][10],
+        reviews_pickle[3][15],
+        reviews_pickle[3][17],
+        reviews_pickle[3][21],
+        reviews_pickle[3][22],
+        reviews_pickle[3][24],
+        reviews_pickle[3][27],
+        reviews_pickle[3][28],
+        reviews_pickle[3][33],
+        reviews_pickle[3][36],
+        reviews_pickle[3][39],
+        reviews_pickle[3][40],
+        reviews_pickle[3][60],
+        reviews_pickle[3][62],
+        reviews_pickle[3][67],
+        reviews_pickle[3][68],
+        reviews_pickle[3][69],
+        reviews_pickle[3][71],
+        reviews_pickle[3][72],
+        reviews_pickle[3][28],
+        reviews_pickle[3][28],
+        reviews_pickle[3][28],
+        reviews_pickle[3][28],
+        reviews_pickle[3][28],
+        reviews_pickle[3][28]
+    ]
+    ratings_4 = [
+        reviews_pickle[4][121],
+        reviews_pickle[4][123],
+        reviews_pickle[4][124],
+        reviews_pickle[4][125],
+        reviews_pickle[4][126],
+        reviews_pickle[4][129],
+        reviews_pickle[4][46],
+        reviews_pickle[4][139],
+        reviews_pickle[4][142],
+        reviews_pickle[4][144],
+        reviews_pickle[4][153],
+        reviews_pickle[4][155],
+        reviews_pickle[4][156],
+        reviews_pickle[4][180],
+        reviews_pickle[4][183],
+        reviews_pickle[4][185],
+        reviews_pickle[4][186],
+        reviews_pickle[4][187],
+        reviews_pickle[4][190],
+        reviews_pickle[4][193],
+        reviews_pickle[4][195],
+        reviews_pickle[4][108],
+        reviews_pickle[4][129],
+        reviews_pickle[4][201],
+        reviews_pickle[4][204],
+        reviews_pickle[4][162],
+        reviews_pickle[4][163],
+        reviews_pickle[4][165],
+        reviews_pickle[4][166],
+        reviews_pickle[4][167],
+        reviews_pickle[4][169],
+        reviews_pickle[4][4],
+        reviews_pickle[4][174],
+        reviews_pickle[4][177],
+        reviews_pickle[4][178],
+        reviews_pickle[4][46]
+    ]
+    ratings_5 = [
+        reviews_pickle[5][1000],
+        reviews_pickle[5][1005],
+        reviews_pickle[5][577],
+        reviews_pickle[5][1014],
+        reviews_pickle[5][1017],
+        reviews_pickle[5][1024],
+        reviews_pickle[5][1025],
+        reviews_pickle[5][1029],
+        reviews_pickle[5][1032],
+        reviews_pickle[5][1038],
+        reviews_pickle[5][1040],
+        reviews_pickle[5][1049],
+        reviews_pickle[5][1055],
+        reviews_pickle[5][1056],
+        reviews_pickle[5][1058],
+        reviews_pickle[5][1064],
+        reviews_pickle[5][1066],
+        reviews_pickle[5][1076],
+        reviews_pickle[5][1077],
+        reviews_pickle[5][1084],
+        reviews_pickle[5][1085],
+        reviews_pickle[5][1086],
+        reviews_pickle[5][1091],
+        reviews_pickle[5][1094],
+        reviews_pickle[5][273]
+    ]
 
     json_helper(bug_1, 1, "bug report")
+    json_helper(bug_2, 2, "bug report")
     json_helper(bug_3, 3, "bug report")
     json_helper(bug_4, 4, "bug report")
     json_helper(feature_1, 1, "feature request")
+    json_helper(feature_2, 2, "feature request")
     json_helper(feature_3, 3, "feature request")
     json_helper(feature_4, 4, "feature request")
+    json_helper(feature_5, 5, "feature request")
     json_helper(user_ex_1, 1, "user experience")
+    json_helper(user_ex_2, 2, "user experience")
     json_helper(user_ex_3, 3, "user experience")
     json_helper(user_ex_4, 4, "user experience")
+    json_helper(user_ex_5, 5, "user experience")
     json_helper(ratings_1, 1, "ratings")
+    json_helper(ratings_2, 2, "ratings")
     json_helper(ratings_3, 3, "ratings")
     json_helper(ratings_4, 4, "ratings")
+    json_helper(ratings_5, 5, "ratings")
 
     with open("classified_reviews.json", "w") as out:
         json.dump(reviews_json, out, ensure_ascii=False, indent=4)
